c2285640fafaf4a2e9457668595e0b9754e5acf9
Nguyễn Trung Dũng <dungnt100190@gmail.com>
2018-09-05 14:56:00 +0700

    Commit

--- Kiss4WebTest/Kiss4Web.Test.TestInfrastructure/TestDataManager.cs
+++ Kiss4WebTest/Kiss4Web.Test.TestInfrastructure/TestDataManager.cs
@@ -5,7 +5,6 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Data.Entity;
 using System.Globalization;
